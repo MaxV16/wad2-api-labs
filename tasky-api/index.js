a4201c92816fc748aa4c860f6d217d88c877284b
MaxV16 <maks.karakulinn@gmail.com>
2025-11-18 11:46:11 +0000

    Mongo lab; Included Mongoose

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,16 +1,24 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+import './db';
+
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.static('public'));
 
 const port = process.env.PORT;
 
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
